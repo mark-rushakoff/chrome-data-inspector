d2de9a52de668c33a7ed9442e2932f75a46be526
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-04-09 21:21:11 -0700

    kinda sorta working in app

--- inspector.js
+++ inspector.js
@@ -1,8 +1,44 @@
-var parentId = chrome.contextMenus.create({
-    title: "Data Inspector",
-    contexts: ["all"]
-});
-
 chrome.extension.onRequest.addListener(function(request) {
-    console.log(request);
-});
+    //console.log("request is ", request);
+
+    if (request.length === 0) {
+        //console.log("setting menu to no-data");
+        chrome.contextMenus.removeAll(menuNoData);
+    } else {
+        //console.log("setting menu to request-data");
+        chrome.contextMenus.removeAll(menuData(request));
+    }
+}, checkFailure);
+
+function menuNoData() {
+    var parentId = menuMainItem(0);
+};
+
+function menuData(request) {
+    return function() {
+        var data, i;
+
+        var parentId = menuMainItem(request.length);
+
+        for (i = 0; i < request.length; i++) {
+            data = request[i];
+            chrome.contextMenus.create({
+                title: "" + data.distance + " - " + data.data + ": " + data.value,
+                parentId: parentId
+            }, checkFailure);
+        }
+    }
+};
+
+function menuMainItem(num) {
+    return chrome.contextMenus.create({
+        title: "Data Inspector - " + num,
+        contexts: ["all"]
+    }, checkFailure);
+}
+
+function checkFailure() {
+    if (chrome.extension.lastError) {
+        console.warn(chrome.extension.lastError);
+    }
+}
--- watch_right_clicks.js
+++ watch_right_clicks.js
@@ -1,11 +1,9 @@
-"use strict";
-
 if (chrome && chrome.extension) {
-    var i = 0;
-    window.addEventListener('mousedown', function(e) {
+    window.addEventListener("mousedown", function(e) {
         var isRightClick = (e.which === 3);
         if (isRightClick) {
-            chrome.extension.sendRequest({value: ++i});
+            //console.log(nodeToDataObjects(e.target));
+            chrome.extension.sendRequest(nodeToDataObjects(e.target));
         }
     });
 }
