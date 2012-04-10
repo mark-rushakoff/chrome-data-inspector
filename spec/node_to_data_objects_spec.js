1b438f110a396a8d428c01a80f7df27e12ce5381
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-04-09 19:13:09 -0700

    More specs

--- spec/node_to_data_objects_spec.js
+++ spec/node_to_data_objects_spec.js
@@ -16,4 +16,39 @@ describe("nodeToDataObjects", function() {
             value: "bar"
         }]);
     });
+
+    it("does not overwrite collected attributes as it walks up the DOM", function() {
+        var $el = $('<div id="outer" data-dog="woof" data-foo="quux"><div id="inner" data-foo="bar"></div></div>');
+        var dataObjects = nodeToDataObjects($el.find("#inner").get(0));
+        expect(dataObjects.length).toBe(3);
+        expect(dataObjects).toContain({
+            distance: 0,
+            data: "foo",
+            value: "bar"
+        });
+        expect(dataObjects).toContain({
+            distance: 1,
+            data: "dog",
+            value: "woof"
+        });
+        expect(dataObjects).toContain({
+            distance: 1,
+            data: "foo",
+            value: "quux"
+        });
+    });
+
+    it("doesn't blow up when a node has no data attributes", function() {
+        var $el = $('<div id="d2" data-dog="woof"><div><div id="d0" data-foo="bar"></div></div></div>');
+        var el = $el.find("#d0").get(0);
+        expect(nodeToDataObjects(el)).toEqual([{
+            distance: 0,
+            data: "foo",
+            value: "bar"
+        }, {
+            distance: 2,
+            data: "dog",
+            value: "woof"
+        }]);
+    });
 });
--- watch_right_clicks.js
+++ watch_right_clicks.js
@@ -1,3 +1,5 @@
+"use strict";
+
 if (chrome && chrome.extension) {
     var i = 0;
     window.addEventListener('mousedown', function(e) {
@@ -9,24 +11,25 @@ if (chrome && chrome.extension) {
 }
 
 function nodeToDataObjects(el) {
-    if (!el) {
-        return [];
-    }
-
     var dataObjects = [],
         distance = 0,
         attr, attrName, i;
 
-    for (i = 0; i < el.attributes.length; i++) {
-        attr = el.attributes[i];
-        attrName = attr.nodeName;
-        if (attrName.indexOf("data-") === 0) {
-            dataObjects.push({
-                data: attrName.substring(5),
-                value: attr.nodeValue,
-                distance: distance,
-            });
+    while (el) {
+        for (i = 0; i < el.attributes.length; i++) {
+            attr = el.attributes[i];
+            attrName = attr.nodeName;
+            if (attrName.indexOf("data-") === 0) {
+                dataObjects.push({
+                    data: attrName.substring(5),
+                    value: attr.nodeValue,
+                    distance: distance,
+                });
+            }
         }
+
+        el = el.parentElement;
+        distance++;
     }
 
     return dataObjects;
